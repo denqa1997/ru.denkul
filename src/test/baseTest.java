9cd57e463360d792e33a239392c20a8a9a1a5392
denqa <denis.kulikov1997@mail.ru>
2025-05-18 23:14:11 +0400

    testcommit

--- src/test/baseTest.java
+++ src/test/baseTest.java
@@ -1,10 +1,14 @@
+import com.codeborne.selenide.SelenideElement;
 import org.junit.jupiter.api.Test;
 
+import static com.codeborne.selenide.Selenide.$x;
 import static com.codeborne.selenide.Selenide.open;
 
 public class baseTest {
 @Test
 void openSite() {
-    open("https://cafekamenka.ru/");
+    open("https://cafekamenka.ru/tihaya");
+    SelenideElement takeATable = $x("//span[@class='label' and contains(text(), 'Заказать столик')]");
+    takeATable.click();
     }
 }
